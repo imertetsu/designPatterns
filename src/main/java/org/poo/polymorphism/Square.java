beef0dd7ee139895a9e548b63f9975b4bbdd5b7e
imertetsu <imertpro@gmail.com>
2024-07-31 23:10:15 -0400

    overloading a method added

--- src/main/java/org/poo/polymorphism/Square.java
+++ src/main/java/org/poo/polymorphism/Square.java
@@ -7,6 +7,13 @@ public class Square extends Figure{
         return side * side;
     }
 
+    public int calculatePerimeter(int side){
+        return 4* side;
+    }
+    public double calculatePerimeter(double side){
+        return 4 * side;
+    }
+
     public static void main(String[] args) {
         Figure square = new Square();
         System.out.println(square.calculateArea(4));
